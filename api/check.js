4caf543eeb1c86baa562096cdfc08ba7009608a4
jeffalo <40470736+jeffalo@users.noreply.github.com>
2020-12-21 22:22:02 +0100

    lowercase

--- api/check.js
+++ api/check.js
@@ -4,5 +4,5 @@ module.exports = async (req, res) => {
     var resp = await fetch('https://scratch.mit.edu/messages/ajax/user-activity/?user=griffpatch&max=6&cache='+Math.random())
     var data = await resp.text()
 
-    res.send(data)
+    res.send(data.toLowerCase())
 }
