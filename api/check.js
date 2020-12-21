9f7b0466fb200e66d3254e64e55c8173eae4bb29
jeffalo <40470736+jeffalo@users.noreply.github.com>
2020-12-21 20:55:47 +0100

    fix

--- api/check.js
+++ api/check.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 module.exports = async (req, res) => {
-    var resp = await fetch('https://scratch.mit.edu/messages/ajax/user-activity/?user=griffpatch&max=6&cache='Math.random())
+    var resp = await fetch('https://scratch.mit.edu/messages/ajax/user-activity/?user=griffpatch&max=6&cache='+Math.random())
     var data = await resp.text()
 
     res.send(data)
