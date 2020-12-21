cfbdccce54ae5e9f279c2215d3f97e80308e36aa
jeffalo <40470736+jeffalo@users.noreply.github.com>
2020-12-21 20:48:35 +0100

    fix

--- api/check.js
+++ api/check.js
@@ -4,7 +4,5 @@ module.exports = async (req, res) => {
     var resp = await fetch('https://scratch.mit.edu/messages/ajax/user-activity/?user=griffpatch&max=6')
     var data = await resp.text()
 
-    res.json({
-      body: data
-    })
+    res.send(data)
 }
